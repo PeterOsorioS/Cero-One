9066829a811c04edaeab36d89800ffff0a3da879
Peter Osorio <PeterMichaelOsorioSantiago@outlook.com>
2025-02-13 11:53:00 -0500

    ajuste

--- src/data/iconos.ts
+++ src/data/iconos.ts
@@ -2,32 +2,32 @@ import { icon } from "../types/index";
 
 export const icons_tech : icon[] = [
     // 🔹 Lenguajes de Programación
-    { name: "Java", image: "./assets/icons/Java.svg" },
-    { name: "Python", image: "../src/assets/icons/Python.svg" },
-    { name: "JavaScript", image: "./src/assets/icons/JavaScript.svg" },
-    { name: "TypeScript", image: "./src/assets/icons/TypeScript.svg" },
-    { name: "Kotlin", image: "./src/assets/icons/Kotlin.svg" },
+    { name: "Java", image: "/assets/icons/Java.svg" },
+    { name: "Python", image: "/assets/icons/Python.svg" },
+    { name: "JavaScript", image: "/assets/icons/JavaScript.svg" },
+    { name: "TypeScript", image: "/assets/icons/TypeScript.svg" },
+    { name: "Kotlin", image: "/assets/icons/Kotlin.svg" },
 
     // 🔹 Frameworks y Librerías Frontend
-    { name: "React", image: "./src/assets/icons/React.svg" },
-    { name: "Vue.js", image: "./src/assets/icons/VueJs.svg" },
-    { name: "AngularJS", image: "./src/assets/icons/AngularJS.svg" },
-    { name: "Blazor", image: "./src/assets/icons/Blazor.svg" },
-    { name: "TailwindCSS", image: "./src/assets/icons/TailwindCSS.svg" },
-    { name: "Bootstrap", image: "./src/assets/icons/Bootstrap5.svg" },
+    { name: "React", image: "/assets/icons/React.svg" },
+    { name: "Vue.js", image: "/assets/icons/VueJs.svg" },
+    { name: "AngularJS", image: "/assets/icons/AngularJS.svg" },
+    { name: "Blazor", image: "/assets/icons/Blazor.svg" },
+    { name: "TailwindCSS", image: "/assets/icons/TailwindCSS.svg" },
+    { name: "Bootstrap", image: "/assets/icons/Bootstrap5.svg" },
 
     // 🔹 Backend y Frameworks de Desarrollo
-    { name: ".NET", image: "./src/assets/icons/.NET.svg" },
-    { name: "Django", image: "./src/assets/icons/Django.svg" },
-    { name: "Node.js", image: "./src/assets/icons/Nodejs.svg" },
+    { name: ".NET", image: "/assets/icons/.NET.svg" },
+    { name: "Django", image: "/assets/icons/Django.svg" },
+    { name: "Node.js", image: "/assets/icons/Nodejs.svg" },
 
     // 🔹 Bases de Datos
-    { name: "MongoDB", image: "./src/assets/icons/MongoDB.svg" },
-    { name: "MariaDB", image: "./src/assets/icons/MariaDB.svg" },
-    { name: "PostgreSQL", image: "./src/assets/icons/PostgreSQL.svg" },
-    { name: "Oracle", image: "./src/assets/icons/Oracle.svg" },
+    { name: "MongoDB", image: "/assets/icons/MongoDB.svg" },
+    { name: "MariaDB", image: "/assets/icons/MariaDB.svg" },
+    { name: "PostgreSQL", image: "/assets/icons/PostgreSQL.svg" },
+    { name: "Oracle", image: "/assets/icons/Oracle.svg" },
 
     // 🔹 Tecnologías Web y Herramientas
-    { name: "HTML5", image: "./src/assets/icons/HTML5.svg" },
-    { name: "CSS3", image: "./src/assets/icons/CSS3.svg" },
+    { name: "HTML5", image: "/assets/icons/HTML5.svg" },
+    { name: "CSS3", image: "/assets/icons/CSS3.svg" },
 ];
